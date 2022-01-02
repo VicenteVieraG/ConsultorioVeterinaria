263683399463f2c922941e36094bf2cf1a8d4818
Vicente Viera <plantedcoot706@gmail.com>
2022-01-01 23:53:41 -0600

    first commit with tailwind working

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header.jsx";
+import Formulario from "./components/Formulario.jsx";
+import ListadoPacientes from "./components/ListadoPacientes.jsx";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Formulario/>
+      <ListadoPacientes/>
+    </>
+  )
+}
+
+export default App
+
+
+
+  
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Formulario = () => {
+    return (
+        <h2 className="font-serif">
+            Formulario
+        </h2>
+    )
+}
+
+export default Formulario;
