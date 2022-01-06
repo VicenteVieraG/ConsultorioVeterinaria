381deaeed2bd5fa4c889eded023c3cf5ea416e4c
Vicente Viera <plantedcoot706@gmail.com>
2022-01-05 22:27:06 -0600

    Trying props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from "./components/Header.jsx";
 import Formulario from "./components/Formulario.jsx";
 import ListadoPacientes from "./components/ListadoPacientes.jsx";
+import {useState} from "react";
 
 function App() {
+	const [pacientes,setPacientes]=useState([]);
+
+	const tuv = (valor) => console.log(valor);
 	return (
 		<div className="container mx-auto mt-20">
-			<Header/>
+			<Header
+				tuv={tuv}
+			/>
 			<div className="mt-12 md:flex">
 				<Formulario/>
 				<ListadoPacientes/>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
 const Formulario = () => {
+    const [nombre, setNombre] = useState("")
+    const [propietario, setPropietario] = useState("")
+    const [email, setEmail] = useState("")
+    const [fecha, setFecha] = useState("")
+    const [sintomas, setSintomas] = useState("")
+    const [error, setError] = useState(false)
+    
+    const handelSubmit= (e) =>{
+        e.preventDefault();
+
+        //validacion del formulario
+        if([nombre,propietario,email,fecha,sintomas].includes("")){
+            console.log("Hay almenos un campo vacio")
+            setError(true);
+            return;
+        }
+
+        setError(false);
+    }
+
     return (
-        <div className="md:w-1/2 lg:w-2/5">
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
             <p className="text-lg mt-5 text-center mb-10">
@@ -10,7 +30,10 @@ const Formulario = () => {
                 <span className="text-indigo-600 font-bold">Administralos</span>
             </p>
 
-            <form className="bg-white shadow-md rounded-lg py-10 px-10 mb-10">
+            <form 
+                onSubmit={handelSubmit}
+                className="bg-white shadow-md rounded-lg py-10 px-10 mb-10">
+                {error && <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md"><p>Todos los campos son obligatorios</p></div>}
                 <div className="mb-5">
                     <label
                         htmlFor="mascota" 
@@ -22,6 +45,8 @@ const Formulario = () => {
                         type="text" 
                         placeholder="Nombre de la mascota" 
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
                     />
                 </div>
 
@@ -36,6 +61,8 @@ const Formulario = () => {
                         type="text" 
                         placeholder="Nombre del propietario" 
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={propietario}
+                        onChange={(e) => setPropietario(e.target.value)}
                     />
                 </div>
 
@@ -50,6 +77,8 @@ const Formulario = () => {
                         type="email" 
                         placeholder="Email conacto propietario" 
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
 
@@ -63,6 +92,8 @@ const Formulario = () => {
                         id="alta" 
                         type="date" 
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={fecha}
+                        onChange={(e) => setFecha(e.target.value)}
                     />
                 </div>
 
@@ -76,6 +107,8 @@ const Formulario = () => {
                         id="sintomas"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         placeholder="Describe los sintomas"
+                        value={sintomas}
+                        onChange={(e) => setSintomas(e.target.value)}
                     />
                 </div>
 
